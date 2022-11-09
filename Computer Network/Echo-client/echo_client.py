8eb95392619b335e3b4cbd2b4f2d9b65e412a01b
Md Rowshon Ul Islam <72875766+rakibphero@users.noreply.github.com>
2022-11-10 00:20:29 +0600

    Add files via upload
    computer network python code added

---
+++ Computer Network/Echo-client/echo_client.py
@@ -0,0 +1,18 @@
+# echo-client.py
+
+import socket
+from turtle import st
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    message=int(input("Write a msg: ")) + 2
+    #message=input("Write a msg: ")
+    s.sendall(str(message).encode())
+    print("original Message",int(message))
+    #print("original Message", message)
+    data = s.recv(1024)
+
+print(f"Echo From Server {data.decode()}")
